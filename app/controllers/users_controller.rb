b80674873b0a204343c69fcad60539b4dbfd9f74
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-07-09 21:25:01 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    render '/signup'
+  end
+
+  def create
+    return redirect_to(controller: 'users',
+    action: 'new') if params[:user][:password] != params[:user][:password_confirmation]
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+  end
+
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
